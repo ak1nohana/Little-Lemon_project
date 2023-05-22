f161c60d5da9da72ae6bfdf933a1b7593ea4063d
ak1nohana <124366629+ak1nohana@users.noreply.github.com>
2023-05-22 16:21:10 +0300

    Update Routing.js

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -10,7 +10,6 @@ import Menu from "./pages/Menu";
 export default function Routing() {
   return (
     <Routes>
-      <Route path="" element={<Home />} />
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/menu" element={<Menu/>}/>
